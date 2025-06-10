479f618b0ddafb7d0e1731571e546680ff15d370
Bantu-art <brianbaraza40@gmail.com>
2025-06-11 00:52:56 +0300

    Refactor command execution in CommandHandler to support argument parsing

--- winux-shell/src/utils/command_handler.py
+++ winux-shell/src/utils/command_handler.py
@@ -1,13 +1,20 @@
 from commands.pwd import PWD
+from commands.ls import LS
 
 class CommandHandler:
     def __init__(self):
         self.commands = {
-            'pwd': PWD()
+            'pwd': PWD(),
+            'ls': LS()
         }
 
     def execute(self, cmd):
-        command = self.commands.get(cmd)
+        # Split command and arguments
+        parts = cmd.split()
+        command_name = parts[0]
+        args = parts[1:] if len(parts) > 1 else None
+
+        command = self.commands.get(command_name)
         if command:
-            return command.execute()
+            return command.execute(args)
         return "Command not recognized."
